1c790b929bc3321195086d90a55581df088ba005
Matt Camilli <mlcamilli@gmail.com>
2015-03-11 13:31:22 -0400

    added license and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+requires = [
+    'requests==2.5.3',
+]
+
+
+setup(
+    name='intercom',
+    author="TrackMaven Engineering",
+    author_email="engineering@trackmaven.com",
+    version='0.0.1',
+    description="",
+    url="https://github.com/TrackMaven/intercom",
+    install_requires=requires,
+    license="MIT",
+)
